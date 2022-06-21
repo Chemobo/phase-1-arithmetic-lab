bcadd0b022e62be99661f69253cee04aa59d3635
James Chemobo <jkchemobo@gmail.com>
2022-06-21 11:01:07 +0300

    commit tree c512d9364856d7a3058245f7aec2bcb4132c3f90

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+const random = (Math.floor(Math.random() * 10 ) +1);
+
+const mod = 19 % 5;
+let num3 = 19;
+let num4 = 5; 
+
+const x = 15; 
+const y = 20;
+const max = Math.max(x,y);
+
+
+
+
+
+
+
+
+
+
